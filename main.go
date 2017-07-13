83c55636082735e089d18b4371f9adaa0310b7e7
Matt Weagle <mweagle@gmail.com>
2017-07-12 23:04:51 -0700

    Update main.go

--- main.go
+++ main.go
@@ -13,40 +13,69 @@ import (
 )
 
 // Standard AWS λ function
-func helloWorld(event *json.RawMessage,
+func helloXRay(event *json.RawMessage,
 	context *sparta.LambdaContext,
 	w http.ResponseWriter,
 	logger *logrus.Logger) {
 	fmt.Fprint(w, "Hello XRay World ☢️")
 }
 
+func xRaySampleFunctions() []*sparta.LambdaAWSInfo {
+	// Default options for all lambda functions
+	sampleFunctions := make([]*sparta.LambdaAWSInfo, 0)
+
+	lambdaMaker := func(xRayIndex int) sparta.LambdaFunction {
+		return func(event *json.RawMessage,
+			context *sparta.LambdaContext,
+			w http.ResponseWriter,
+			logger *logrus.Logger) {
+			fmt.Fprintf(w, "Hello World from XRay %d: ☢️", xRayIndex)
+		}
+	}
+
+	// Create 6 functions
+	for i := 0; i != 6; i++ {
+		lambdaOptions := &sparta.LambdaFunctionOptions{
+			MemorySize: 256,
+			TracingConfig: &gocf.LambdaFunctionTracingConfig{
+				Mode: gocf.String("Active"),
+			},
+			Tags: map[string]string{
+				"myAccounting": "tag",
+			},
+			SpartaOptions: &sparta.SpartaOptions{
+				Name: fmt.Sprintf("XRaySampleFunction%d", i),
+			},
+		}
+		lambdaFn := sparta.NewLambda(sparta.IAMRoleDefinition{},
+			lambdaMaker(i),
+			lambdaOptions)
+		sampleFunctions = append(sampleFunctions, lambdaFn)
+	}
+	return sampleFunctions
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 func main() {
-	lambdaOptions := &sparta.LambdaFunctionOptions{
-		MemorySize: 256,
-		TracingConfig: &gocf.LambdaFunctionTracingConfig{
-			Mode: gocf.String("Active"),
-		},
-		SpartaOptions: &sparta.SpartaOptions{
-			Name: "SpartaXRay",
-		},
-	}
-	// Add tags
-	lambdaOptions.Tags = map[string]string{"special": "tag"}
-	lambdaFn := sparta.NewLambda(sparta.IAMRoleDefinition{},
-		helloWorld,
-		lambdaOptions)
+	// Setup some sample functions
+	lambdaFunctions := xRaySampleFunctions()
 
 	// Sanitize the name so that it doesn't have any spaces
 	stackName := spartaCF.UserScopedStackName("SpartaXRay")
-	var lambdaFunctions []*sparta.LambdaAWSInfo
-	lambdaFunctions = append(lambdaFunctions, lambdaFn)
-	err := sparta.Main(stackName,
+
+	// Setup the DashboardDecorator lambda hook
+	workflowHooks := &sparta.WorkflowHooks{
+		ServiceDecorator: sparta.DashboardDecorator(lambdaFunctions, 60),
+	}
+
+	err := sparta.MainEx(stackName,
 		stackName,
 		lambdaFunctions,
 		nil,
-		nil)
+		nil,
+		workflowHooks,
+		false)
 	if err != nil {
 		os.Exit(1)
 	}
